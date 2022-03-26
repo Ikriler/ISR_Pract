66dcb6ee10140eb3f8be70ec8cc3e9ed7675e2dc
Ilya Gorev <isip_i.a.gorev@mpt.ru>
2022-03-26 10:52:14 +0300

    add letter.py

---
+++ letter.py
@@ -0,0 +1,13 @@
+def StrCounter():
+    try:
+        str = input("Введите строку: ")
+        space = str.count(" ")
+        comma = str.count(",")
+        strlen = len(str)
+        print("Количество символов: ", strlen)
+        print("Количество пробелов: ", space)
+        print("Количество запятых: ", comma)
+    except Exception:
+        print("Ошибка ввода данных")
+
+StrCounter()
